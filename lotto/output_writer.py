faedfd493de50a8f79dd8bf1ec2b9ac44a3bf1ee
rmayherr <roland.mayherr@gmail.com>
2020-08-25 13:59:24 +0200

    Final pep8 fix

--- lotto/output_writer.py
+++ lotto/output_writer.py
@@ -3,9 +3,16 @@ from configparser import ConfigParser
 from downloader import get_from_config
 from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl import Workbook
-from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font
+from openpyxl.styles import (
+                                PatternFill,
+                                Border,
+                                Side,
+                                Alignment,
+                                Protection,
+                                Font)
 import os
 import pandas
+
 
 def write_to_xls_file(*args):
     """Write dataframe to spreadsheet"""
@@ -14,13 +21,13 @@ def write_to_xls_file(*args):
         print(f'Writing to {get_from_config("xls_output")}...', end='')
         if os.path.exists(get_from_config("xls_output")):
             os.remove(get_from_config("xls_output"))
-        font_head = Font(name = "Arial",
-                    size = "12",
-                    bold = True,
-                    color = 'FFFFFF'
-                    )
-        fill_head_green = PatternFill(start_color='09A014', end_color='09A014', \
-                              fill_type='solid')
+        font_head = Font(name="Arial",
+                         size="12",
+                         bold=True,
+                         color='FFFFFF')
+        fill_head_green = PatternFill(start_color='09A014',
+                                      end_color='09A014',
+                                      fill_type='solid')
         file = get_from_config('xls_output')
         wb = Workbook()
         for pos, dataframe in enumerate(args):
@@ -35,5 +42,5 @@ def write_to_xls_file(*args):
     except Exception as e:
         print(f'Error occurred while writing to file! \n\t {e}')
     else:
-        wb.save(filename = file)
+        wb.save(filename=file)
         print(f'Done.')
